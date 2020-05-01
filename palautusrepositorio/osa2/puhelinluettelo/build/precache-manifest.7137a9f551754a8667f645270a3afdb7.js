89463320bb55750445c8f8e63998eac4304e41b6
Kiilas Kadri <kiilas@vdi-cubic-003.ad.helsinki.fi>
2020-05-01 22:10:18 +0300

    bla

---
+++ palautusrepositorio/osa2/puhelinluettelo/build/precache-manifest.7137a9f551754a8667f645270a3afdb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338c6e8a09e6607ed7c62c69b4cff00b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d15d937a8f441cea23d",
+    "url": "/static/css/main.8ed0eddc.chunk.css"
+  },
+  {
+    "revision": "581509edc098e8f19e97",
+    "url": "/static/js/2.13a90021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13a90021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d15d937a8f441cea23d",
+    "url": "/static/js/main.907a5e2b.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
--- palautusrepositorio/osa2/puhelinluettelo/src/App.js
+++ palautusrepositorio/osa2/puhelinluettelo/src/App.js
@@ -61,7 +61,7 @@ const App = () => {
   const handleFilterChange = (event) => setFilter(event.target.value.toLowerCase())
 
   const handleDeleteClick = (event) => {
-    const id = Number(event.target.value)
+    const id = event.target.value
     const person = persons.find(p => p.id === id)
     if (window.confirm(`Remove ${person.name} ?`)) {
       personService
